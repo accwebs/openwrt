8e6173b53eec319655ed6372d35c7a786c100201
Aaron Curley <accwebs@gmail.com>
2025-07-31 21:05:21 -0400

    Modify D-Link DSP-W215 (a1 board variant) to use an IMAGE_SIZE of 16MB to allow building larger images flashed via sysupgrade

--- target/linux/ath79/image/tiny.mk
+++ target/linux/ath79/image/tiny.mk
@@ -108,7 +108,7 @@ define Device/dlink_dsp-w215-a1
   DEVICE_VENDOR := D-Link
   DEVICE_MODEL := DSP-W215
   DEVICE_VARIANT := A1/A2
-  IMAGE_SIZE := 4544k
+  IMAGE_SIZE := 12800k
   DEVICE_PACKAGES := -kmod-mdio -kmod-mii -luci-proto-ppp -ppp \
 	-ppp-mod-pppoe -swconfig
   IMAGES += factory.bin
