48e6830a904c81410f7f4a371046b4485df39751
Aaron Curley <accwebs@gmail.com>
2025-08-29 21:21:19 -0400

    Add "A2" to DSP-W215 A1 model string.
    This MAY be conceptually incorrect if A2 GPIOs are different from A1, but talking with Sebastian Schaper it doesn't sound like he was TOO confident in the GPIOs for his A1 .dts and for the most part they match my A2 device (there are two errors, fixes to come)

--- target/linux/ath79/image/tiny.mk
+++ target/linux/ath79/image/tiny.mk
@@ -107,7 +107,7 @@ define Device/dlink_dsp-w215-a1
   SOC := ar9331
   DEVICE_VENDOR := D-Link
   DEVICE_MODEL := DSP-W215
-  DEVICE_VARIANT := A1
+  DEVICE_VARIANT := A1/A2
   IMAGE_SIZE := 4544k
   DEVICE_PACKAGES := -kmod-mdio -kmod-mii -luci-proto-ppp -ppp \
 	-ppp-mod-pppoe -swconfig
